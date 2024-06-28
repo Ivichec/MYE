6cb4a93d4d69fd9b10594c77c1f004e40becd192
Ivan Checa <124150320+Ivichec@users.noreply.github.com>
2024-06-28 13:31:48 +0200

    .

--- mye/codigo/views.py
+++ mye/codigo/views.py
@@ -6,3 +6,5 @@ def index1(request):
     return render(request, "hospitales/Inicio.html")
 def index2(request):
     return render(request, "hospitales/Inicio.html")
+def index3(request):
+    return render(request, "hospitales/Inicio.html")
