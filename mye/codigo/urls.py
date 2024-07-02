85c50f1888b28a6710a1f9248b4a3f7e184c055d
nachox99 <999nacho@gmail.com>
2024-07-02 10:43:22 +0200

    aljdfoij

--- mye/codigo/urls.py
+++ mye/codigo/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('inicio', views.index, name='index'),
     path('a_Login', views.Ir_a_login, name='iralogin'),
     path('a_que_es_mye', views.Ir_a_que_es_mye, name='iraqueesmye'),
+    path('BotonUser', views.CompruebaPass, name='comprueba'),
+    path('NuevoUser', views.CrearUsuario, name='NuevoUser'),
 ]
--- mye/codigo/views.py
+++ mye/codigo/views.py
@@ -7,6 +7,18 @@ def index(request):
 def Ir_a_login(request):
     return render(request, "inicial/login.html")
 
+def CrearUsuario(request):
+    return render(request, "inicial/registrar.html")
+
 def Ir_a_que_es_mye(request):
     # pendiente desarrollar función que llame al video de presentación
     pass
+
+def CompruebaPass(request):
+    mail = request.POST['txtemail']
+    emple = Empleado()
+    cursor = emple.devolverdato(mail)
+    contexto = {
+        'listado_empleados': cursor
+    }
+    return render(request, "deportes/Empleados.html", contexto)
