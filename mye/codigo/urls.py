d0c0121d8b78b92a5ba62e6d2949560faf65c944
nachox99 <999nacho@gmail.com>
2024-07-02 09:26:52 +0200

    algo

--- mye/codigo/urls.py
+++ mye/codigo/urls.py
@@ -1,1 +1,9 @@
-from x porque si
+from django.urls import path
+
+from codigo import (views)
+
+urlpatterns = [
+    path('inicio', views.index, name='index'),
+    path('a_Login', views.Ir_a_login, name='iralogin'),
+    path('a_que_es_mye', views.Ir_a_que_es_mye, name='iraqueesmye'),
+]
--- mye/codigo/views.py
+++ mye/codigo/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+#from codigo.models import AltaEmp
 
-# Create your views here.
-def defdef():
+def index(request):
+    return render(request, "inicial/inicio.html")
+
+def Ir_a_login(request):
+    return render(request, "inicial/login.html")
+
+def Ir_a_que_es_mye(request):
+    # pendiente desarrollar función que llame al video de presentación
