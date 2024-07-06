89ffcb82b75e0308aa7fd17df07483e6091dbd37
Gema Fernández <gema.gcloud@gmail.com>
2024-07-06 21:09:52 +0200

    pequeñas correcciones

--- mye/codigo/urls.py
+++ mye/codigo/urls.py
@@ -4,19 +4,20 @@ from codigo import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+  # path('a_que_es_mye', views.Ir_a_que_es_mye, name='iraqueesmye'),
     path('a_Login', views.Ir_a_login, name='iralogin'),
-    # path('a_que_es_mye', views.Ir_a_que_es_mye, name='iraqueesmye'),
+    path('menuIni', views.menuIni, name='menuIni'),
+    path('NuevoUser', views.CrearUsuario, name='NuevoUser'),
+    path('BotonUser', views.CompruebaPass, name='BotonUser'),
+    path('darAlta', views.darAlta, name='darAlta'),
+    path('verDiccionario', views.verDiccionario, name='verDiccionario'),
     path('Diccionarios', views.Diccionarios, name='Diccionarios'),
     path('formularioDic', views.formularioDiccionario, name='iraformularioDic'),
     path('formularioDicPost', views.formularioDiccionarioPost, name='iraformularioDicPost'),
-    path('NuevoUser', views.CrearUsuario, name='NuevoUser'),
-    path('darAlta', views.darAlta, name='darAlta'),
-    path('BotonUser', views.CompruebaPass, name='BotonUser'),
-    path('cerrarSesion', views.cerrarSesion, name='cerrarSesion'),
     path('Tests', views.Tests, name='Tests'),
     path('crearTests', views.crearTests, name='crearTests'),
     path('enviarTest', views.enviarTest, name='enviarTest'),
-    path('menuIni', views.menuIni, name='menuIni'),
-    path('verDiccionario', views.verDiccionario, name='verDiccionario'),
+    path('cerrarSesion', views.cerrarSesion, name='cerrarSesion'),
+
     path('prueba', views.prueba, name='prueba'),
 ]
